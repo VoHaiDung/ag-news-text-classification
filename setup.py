77e2112abb365d145e34d628dc893488421a7fea
Võ Hải Dũng <106304280+VoHaiDung@users.noreply.github.com>
2025-07-08 23:07:20 +0700

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="ag_news_classification",
     version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(include=["src", "src.*"]),
     python_requires=">=3.9",
 )
